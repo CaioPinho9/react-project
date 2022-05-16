48b300b05d986a0031dbfcf72f401606fb7877e7
CaioPinho9 <69582667+CaioPinho9@users.noreply.github.com>
2022-05-16 19:24:08 -0300

    Initial commit

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+function HelloWorld({nome,adjetivo}) {
+    return(
+        <div>
+            <p>Hello World, how are you {nome}?</p>
+            <p>You are {adjetivo}</p>
+            
+        </div>
+    )
+}
+export default HelloWorld
